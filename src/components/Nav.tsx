23b4ba754628af36900424b04c83058f6ea56ef7
Hamza Simsek <hamzas4011@hotmail.com>
2026-08-31 14:33:36 +0200

    fixed the navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 export default function Nav() {
     return (
-        <nav className="bg-green 100">
+        <nav className="bg-green 100 flex items-center justify-between px-8 py-2">
             <span className="text-2xl">Din Blomst</span>
             <a href="#om oss" className="font-body hover:text-sage"> Om oss</a>
         </nav>
