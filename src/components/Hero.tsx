3dd11e9f5b5996dd829fa8bfd75a2670a8bcb648
Hamza Simsek <hamzas4011@hotmail.com>
2026-09-02 22:51:04 +0200

    imported a picture

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
     return (
         <section className="h-screen">
             <h1>Welcome to the Hero component</h1>
-            <img src={flowerImage} alt="Red beutiful flower" className="w-full h-full object-cover" />
+            <img src={flowerImage} alt="Red beautiful flower" className="md:h-auto"/>
         </section>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,6 @@ export default function Home() {
     return (
         <div className="items-center">
             <Hero />
-            <h1>Welcome to the Home page</h1>
         </div>
     )
 }
