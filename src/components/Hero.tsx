b0e70141d9eece7a2a3953099cb51c45cb93807a
Hamza Simsek <hamzas4011@hotmail.com>
2026-09-02 21:08:42 +0200

    Import and export of Hero file

---
+++ src/components/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+    return (
+        <div className="hero">
+            <h1>Welcome to the Hero component</h1>
+        </div>
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
+import Hero from "../components/Hero";
+
 export default function Home() {
     return (
         <div className="items-center">
+            <Hero />
             <h1>Welcome to the Home page</h1>
         </div>
     )
