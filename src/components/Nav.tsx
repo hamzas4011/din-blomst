7d705245519374c32488bd3c4f5ac7a734e59f1e
Hamza Simsek <hamzas4011@hotmail.com>
2026-09-03 09:58:11 +0200

    Added better design to navbar and footer

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,10 @@
 export default function Nav() {
     return (
-        <nav className="bg-green-100 flex items-center justify-between px-8 py-2">
-            <span className="text-2xl">Din Blomst</span>
-            <a href="#om oss" className="font-body hover:text-sage"> Om oss</a>
+        <nav className="bg-green-100 flex items-center justify-between px-8 py-4">
+            <span className="font-heading text-2xl text-charcoal">Din Blomst</span>
+            <a href="#om-oss" className="font-body text-sage-dark hover:text-sage transition-colors">
+                Om oss
+            </a>
         </nav>
     )
 }
