5c18ec3ab1636e8271954792cf0828baac44b999
Hamza Simsek <hamzas4011@hotmail.com>
2026-09-07 11:17:05 +0200

    color changes to the navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,25 +3,25 @@ import { Link } from "react-router-dom";
 export default function Nav() {
     return (
         <nav className="bg-green-100 flex items-center justify-between px-8 py-4">
-            <Link to="/" className="font-heading text-2xl text-green-950">
+            <Link to="/" className="font-heading text-2xl text-black">
                 Din Blomst
             </Link>
             <div className="flex gap-6">
                 <Link
                     to="/om-oss"
-                    className="font-body text-green-950 hover:text-green-800 transition-colors underline underline-offset-4"
+                    className=" hover:text-green-500"
                 >
                     Om oss
                 </Link>
                 <Link
                     to="/sortiment"
-                    className="font-body text-green-950 hover:text-green-800 transition-colors underline underline-offset-4"
+                    className=" hover:text-green-500"
                 >
                     Sortiment
                 </Link>
                 <Link
                     to="/cart"
-                    className="font-body text-green-950 hover:text-green-800 transition-colors underline underline-offset-4"
+                    className=" hover:text-green-500"
                 >
                     Handlekurv
                 </Link>
