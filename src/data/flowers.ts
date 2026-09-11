bf7af382073e816728d0352c80fbcc16314bb2af
Hamza Simsek <hamzas4011@hotmail.com>
2026-09-11 23:02:47 +0200

    import of the flowers pictures

--- src/data/flowers.ts
+++ src/data/flowers.ts
@@ -1,3 +1,18 @@
+import flower1 from "../assets/flower-1.jpg";
+import flower2 from "../assets/flower-2.jpg";
+import flower3 from "../assets/flower-3.jpg";
+import flower4 from "../assets/flower-4.jpg";
+import flower5 from "../assets/flower-5.jpg";
+import flower6 from "../assets/flower-6.jpg";
+import flower7 from "../assets/flower-7.jpg";
+import flower8 from "../assets/flower-8.jpg";
+import flower9 from "../assets/flower-9.jpg";
+import flower10 from "../assets/flower-10.jpg";
+import flower11 from "../assets/flower-11.jpg";
+import flower12 from "../assets/flower-12.jpg";
+
+
+
 const Images = () => {
-    
+
 }
